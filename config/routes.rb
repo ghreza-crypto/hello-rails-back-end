19cb909026025d09b7b6ba41e9c11ef8fdbef4ad
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-12-04 18:54:01 +0430

    Add project controller, rout and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :greetings, only: %i[index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
